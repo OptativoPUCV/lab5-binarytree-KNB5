16b3d08bd738d85817ccac43da955f3b83a29b04
Your Name <you@example.com>
2024-05-11 19:56:12 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -153,7 +153,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     }
     else
     {
-        TreeNode* min = minimum(node->right);
+        TreeNode* min = minimum(node);
         node->pair->key = min->pair->key;
         node->pair->value = min->pair->value;
         removeNode(tree, min);
